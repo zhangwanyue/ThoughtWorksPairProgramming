039dfd1da5b4ffaa6123b7cb1575459c6fd86737
zhangwanyue <744404875@qq.com>
2019-06-12 13:45:37 +0800

    test case readFileSuccess finish

--- src/main/java/com/thoughtworks/gameOfLife/utils/FileUtils.java
+++ src/main/java/com/thoughtworks/gameOfLife/utils/FileUtils.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.gameOfLife.utils;
 
 import com.thoughtworks.gameOfLife.models.CellMatrix;
-import jdk.jfr.events.FileReadEvent;
 
 public class FileUtils {
     public static CellMatrix readFileInitCellMatrix(String filePath){
