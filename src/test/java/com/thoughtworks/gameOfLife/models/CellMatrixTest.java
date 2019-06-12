f15bfdf98a57bd002d1ca9e1f7e41aab6b79df99
zhangwanyue <744404875@qq.com>
2019-06-12 15:12:22 +0800

    finish getSurroundLiveCellNumTest

--- src/test/java/com/thoughtworks/gameOfLife/models/CellMatrixTest.java
+++ src/test/java/com/thoughtworks/gameOfLife/models/CellMatrixTest.java
@@ -1,15 +1,24 @@
 package com.thoughtworks.gameOfLife.models;
 
+import com.thoughtworks.gameOfLife.utils.FileUtils;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CellMatrixTest {
     @BeforeClass
-    public static void init(){
+    public void init(){
 //        System.out.println("init");
     }
-    @Test
-    public void whenReadFromFile(){
 
+    @Test
+    public void getSurroundLiveCellNumTest(){
+        String case1Path = "files/case1.txt";
+        CellMatrix case1CellMatrix = FileUtils.readFileInitCellMatrix(case1Path);
+        int[][] case1Matrix = case1CellMatrix.getMatrix();
+        Assert.assertEquals(CellMatrix.getSurroundLiveCellNum(case1Matrix, 0, 0), 0);
+        Assert.assertEquals(CellMatrix.getSurroundLiveCellNum(case1Matrix, 4, 3), 1);
+        Assert.assertEquals(CellMatrix.getSurroundLiveCellNum(case1Matrix, 4, 4), 3);
+        Assert.assertEquals(CellMatrix.getSurroundLiveCellNum(case1Matrix, 5, 4), 2);
     }
 }
