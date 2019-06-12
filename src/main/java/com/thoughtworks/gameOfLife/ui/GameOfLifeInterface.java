a5be2ef5eda9381046cb8530636c11b6e3a23b74
zhangwanyue <744404875@qq.com>
2019-06-13 00:04:27 +0800

    finish GameOfLifeInterface

--- src/main/java/com/thoughtworks/gameOfLife/ui/GameOfLifeInterface.java
+++ src/main/java/com/thoughtworks/gameOfLife/ui/GameOfLifeInterface.java
@@ -1,57 +1,64 @@
 package com.thoughtworks.gameOfLife.ui;
 
 import com.thoughtworks.gameOfLife.models.CellMatrix;
+import com.thoughtworks.gameOfLife.utils.FileUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.TimeUnit;
 
 public class GameOfLifeInterface extends JFrame {
+    //游戏状态
+    private volatile boolean running = false;
+    //默认动画间隔（默认时长为200）
+    private static int DEFAULT_DURATION = 200;
+    private int duration = DEFAULT_DURATION;
+    //控件
     private JPanel controlPanel;
     private JPanel cellPanel;
     private JButton startGameButton;
     private JButton chooseFileButton;
     private JButton pauseButton;
-    private CellMatrix cellMatrix;
+    private JTextField durationTextField;
+    //cellMatrix
+    private volatile CellMatrix cellMatrix;
 
     public GameOfLifeInterface() throws HeadlessException {
         startGameButton = new JButton("开始游戏");
         startGameButton.setPreferredSize(new Dimension(200, 30));
+        startGameButton.addActionListener(new StartGameActionListener());
         chooseFileButton = new JButton("选择文件");
         chooseFileButton.setPreferredSize(new Dimension(200, 30));
+        chooseFileButton.addActionListener(new ChooseFileActionListener());
         pauseButton = new JButton("暂停游戏");
         pauseButton.setPreferredSize(new Dimension(200, 30));
-
+        pauseButton.addActionListener(new pauseGameActionListener());
+        durationTextField = new JTextField();
+        durationTextField.setPreferredSize(new Dimension(200, 30));
 
         controlPanel = new JPanel();
         controlPanel.add(startGameButton);
         controlPanel.add(chooseFileButton);
         controlPanel.add(pauseButton);
-        controlPanel.setPreferredSize(new Dimension(800, 30));
+        controlPanel.add(durationTextField);
 
-        cellPanel = new JPanel();
-        cellPanel.setLayout(new GridLayout(10, 10));
-        for(int i=0; i<10; i++){
-            for(int j=0; j<10; j++){
-
-            }
-        }
-        this.add("Center", cellPanel);
-
-        this.add(controlPanel);
-        this.setSize(800, 600);
+        getContentPane().add("North", controlPanel);
+        this.setSize(1000, 800);
     }
-    // main class
+
+
     public static void main(String[] args)
     {
-       new GameOfLifeInterface().show();
-
+       new GameOfLifeInterface().setVisible(true);
     }
 
 
     /**
-     * 创建显示的gridlayout布局
+     * 使用cellMatrix创建cellPanel布局
      */
-    private void initGridLayout() {
+    private void setCellPanel() {
         int rows = cellMatrix.getHeight();
         int cols = cellMatrix.getWidth();
         cellPanel = new JPanel();
@@ -60,13 +67,97 @@ public class GameOfLifeInterface extends JFrame {
         int[][] matrix = cellMatrix.getMatrix();
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if(matrix[i][j] == 1){
-
-//                    cellPanel.add(text);
+                JButton jButton = new JButton();
+                if(matrix[i][j]==1){
+                    jButton.setBackground(Color.BLACK);
+                }else{
+                    jButton.setBackground(Color.WHITE);
                 }
-
+                cellPanel.add(jButton);
             }
         }
-        add("Center", cellPanel);
+        this.add(cellPanel);
+        cellPanel.updateUI();
     }
+
+    /**
+     * 显示cellPanel的更新
+     */
+    private void updateCellPanel(){
+        cellMatrix.transform();
+        setCellPanel();
+    }
+
+    /**
+     * 选择文件
+     */
+    private class ChooseFileActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // 暂定游戏/游戏未开始
+            running = false;
+            JFileChooser fileChooser = new JFileChooser(".");
+            fileChooser.setDialogTitle("请选择初始配置文件");
+            int returnVal = fileChooser.showOpenDialog(null);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                //读取文件，初始化cellMatrix
+                String filepath = fileChooser.getSelectedFile().getPath();
+                cellMatrix = FileUtils.readFileInitCellMatrix(filepath);
+                //初始化cellPanel
+                setCellPanel();
+            }
+        }
+    }
+
+    /**
+     * 开始游戏
+     */
+    private class StartGameActionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (!running) {
+                //获取时间
+                try {
+                    duration = Integer.parseInt(durationTextField.getText().trim());
+                } catch (NumberFormatException e1) {
+                    duration = DEFAULT_DURATION;
+                }
+                new Thread(new GameControlTask()).start();
+                running = true;
+            }
+        }
+    }
+
+
+    /**
+     * 暂停游戏
+     */
+    private class pauseGameActionListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            if(running){
+                running = false;
+            }
+        }
+    }
+
+    /**
+     * 运行游戏
+     */
+    private class GameControlTask implements Runnable {
+        @Override
+        public void run() {
+            while (running) {
+                cellMatrix.transform();
+                updateCellPanel();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(duration);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
